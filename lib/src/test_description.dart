295895c0ef6d9939d7cdcad5f4fbad52bdb417fa
Peter von der Ahé <ahe@google.com>
2016-09-20 13:43:04 +0200

    Import rasta/lib/testing.
    R=karlklose@google.com
    
    Review URL: https://codereview.chromium.org/2355643003 .

---
+++ lib/src/test_description.dart
@@ -0,0 +1,40 @@
+// Copyright (c) 2016, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE.md file.
+
+library testing.test_description;
+
+import 'dart:io' show
+    File,
+    FileSystemEntity;
+
+class TestDescription implements Comparable<TestDescription> {
+  final Uri root;
+  final File file;
+  final Uri output;
+
+  TestDescription(this.root, this.file, {this.output});
+
+  Uri get uri => file.uri;
+
+  String get shortName {
+    String baseName = "$uri".substring("$root".length);
+    return baseName.substring(0, baseName.length - ".dart".length);
+  }
+
+  static TestDescription from(
+      Uri root, FileSystemEntity entity, {Pattern pattern}) {
+    if (entity is! File) return null;
+    pattern ??= "_test.dart";
+    String path = entity.uri.path;
+    bool hasMatch = false;
+    if (pattern is String) {
+      if (path.endsWith(pattern)) hasMatch = true;
+    } else if (path.contains(pattern)) {
+      hasMatch = true;
+    }
+    return hasMatch ? new TestDescription(root, entity) : null;
+  }
+
+  int compareTo(TestDescription other) => "$uri".compareTo("${other.uri}");
+}
